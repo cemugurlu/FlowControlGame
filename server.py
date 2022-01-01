42455ae8b5eaeeff88a9ce40a218682ebbdce792
alperenozkaya <ozkayaal@mef.edu.tr>
2022-01-01 22:21:31 +0300

    Update server.py
    server last version

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ buffer_time_limit = 30
 local_store = []
 server_buffer = 8  # KB
 serverPort = 12000
-clientAddress = 0
+clientAddress = gethostname()
 
 # SERVER SOCKET
 serverSocket = socket(AF_INET, SOCK_DGRAM)
@@ -29,7 +29,7 @@ def updateAvailableBuffer(size):
     global server_buffer, clientAddress
     server_buffer += size
     acknowledgedMessageOne = f'ACK {server_buffer}'
-    serverSocket.sendto(acknowledgedMessageOne.encode('UTF-8'), clientAddress)
+    serverSocket.sendto(acknowledgedMessageOne.encode('UTF-8'), (clientAddress, serverPort))
 
 
 def decreaseAvailableBuffer(size):
@@ -40,31 +40,33 @@ def decreaseAvailableBuffer(size):
 def main():
     while 1:
         message, clientAddress = serverSocket.recvfrom(2048)
-        message_array = message.split(' ')
+
+        message_array = (message.decode("utf-8")).split(' ')
+        client_rwnd = message_array[0]
         message_length = message_array[1]
-        decreaseAvailableBuffer(message_length)
-        local_store.append([message_length, time.time()])
+        decreaseAvailableBuffer(int(message_length))
+        local_store.append([int(message_length), time.time()])
 
         # TODO: Bu kısım while döngüsünde sürekli ziyaret ediliyor mu? Kontrol edilmeli
         checkTimer()
 
         if isAutomatic:
             print("auto mod")
-            acknowledgedMessage = f'ACK {server_buffer}'
+            acknowledgedMessage = f'{server_buffer} {message_length}'
             # TODO: Send ACK, Cond, if manual wait for user input from terminal
-            # TODO: Input: Remaining buffer length, ACK or NAK
+            # TODO: Input: Remaining buffer length, ACK or NAK *
         else:
             print("manual mod")
             acknowledgedMessage = f'ACK {server_buffer}'
             # TODO: Get  ACK message from user input
             # TODO: send ACK message
-            # TODO: Input: Remaining buffer length, ACK or NAK
+            # TODO: Input: Remaining buffer length, ACK or NAK *
 
         serverSocket.sendto(acknowledgedMessage.encode('UTF-8'), clientAddress)
 
 
 if __name__ == '__main__':
-    # main()
+    main()
     # TODO: mod selection question. Auto or manual
 
     local_store = [[2, time.time()], [4, time.time()]]
